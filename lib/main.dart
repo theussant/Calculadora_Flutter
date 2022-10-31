f2423257b292086b59e90b66b6a2e89a63d3a34d
Matheus Silva <msantossilva005@gmail.com>
2022-10-31 16:44:40 -0300

    issue resolve

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -99,186 +97,187 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Center(
-            child: Text('Calculadora'),
+          appBar: AppBar(
+            title: const Center(
+              child: Text('Calculadora'),
+            ),
           ),
-        ),
-      ),
+          // ignore: dead_code
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    numero,
+                    style: TextStyle(fontSize: 72),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  GestureDetector(
+                    onTap: () => calcular('Ac'),
+                    child: Text(
+                      'AC',
+                      style: TextStyle(fontSize: 48),
+                    ),
+                  ),
+                  Text(
+                    '',
+                    style: TextStyle(fontSize: 48),
+                  ),
+                  Text(
+                    '',
+                    style: TextStyle(fontSize: 48),
+                  ),
+                  Text(
+                    '',
+                    style: TextStyle(fontSize: 48),
+                  ),
+                  Text(
+                    '<x',
+                    style: TextStyle(fontSize: 48),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  GestureDetector(
+                    onTap: () => calcular('7'),
+                    child: Text(
+                      '7',
+                      style: TextStyle(fontSize: 48),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => calcular('8'),
+                    child: Text(
+                      '8',
+                      style: TextStyle(fontSize: 48),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => calcular('9'),
+                    child: Text(
+                      '9',
+                      style: TextStyle(fontSize: 48),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => calcular('/'),
+                    child: Text(
+                      '/',
+                      style: TextStyle(fontSize: 48),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  GestureDetector(
+                    onTap: () => calcular('4'),
+                    child: Text(
+                      '4',
+                      style: TextStyle(fontSize: 48),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => calcular('5'),
+                    child: Text(
+                      '5',
+                      style: TextStyle(fontSize: 48),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => calcular('6'),
+                    child: Text(
+                      '6',
+                      style: TextStyle(fontSize: 48),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => calcular('x'),
+                    child: Text(
+                      'x',
+                      style: TextStyle(fontSize: 48),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  GestureDetector(
+                    onTap: () => calcular('1'),
+                    child: Text(
+                      '1',
+                      style: TextStyle(fontSize: 48),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => calcular('2'),
+                    child: Text(
+                      '2',
+                      style: TextStyle(fontSize: 48),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => calcular('3'),
+                    child: Text(
+                      '3',
+                      style: TextStyle(fontSize: 48),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => calcular('-'),
+                    child: Text(
+                      '-',
+                      style: TextStyle(fontSize: 48),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  GestureDetector(
+                    onTap: () => calcular('0'),
+                    child: Text(
+                      '0',
+                      style: TextStyle(fontSize: 48),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => calcular(','),
+                    child: Text(
+                      ',',
+                      style: TextStyle(fontSize: 48),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => calcular('='),
+                    child: Text(
+                      '=',
+                      style: TextStyle(fontSize: 48),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => calcular('+'),
+                    child: Text(
+                      '+',
+                      style: TextStyle(fontSize: 48),
+                    ),
+                  ),
+                ],
+              ),
+              Text('Coluna 6'),
+            ],
+          )),
     );
-
-    // ignore: dead_code
-    body:
-    var column = Column(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Text(
-              numero,
-              style: TextStyle(fontSize: 72),
-            ),
-          ],
-        ),
-
-       Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            GestureDetector(
-              onTap: (() => calcular('Ac'),
-              child: Text(
-                'AC',
-                style: TextStyle(fontSize: 48),
-              ),
-            ),
-            Text(
-              '',
-              style: TextStyle(fontSize: 48),
-            ),
-            Text(
-              '',
-              style: TextStyle(fontSize: 48),
-            ),
-            Text(
-              '',
-              style: TextStyle(fontSize: 48),
-            ),
-            Text(
-              '<x',
-              style: TextStyle(fontSize: 48),
-            ),
-          ],
-        )
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            GestureDetector(
-              onTap: (() => calcular('7'),
-              child: Text(
-                '7',
-                style: TextStyle(fontSize: 48),
-              ),
-            ),
-            GestureDetector(
-              onTap: () => calcular('8'),
-              child: Text(
-                '8',
-                style: TextStyle(fontSize: 48),
-              ),
-            ),
-            GestureDetector(
-              onTap:() => calcular('9'),
-              child: Text(
-                '9',
-                style: TextStyle(fontSize: 48),
-              ),
-            ),
-            GestureDetector(
-              onTap:() => calcular('/'),
-              child: Text(
-                '/',
-                style: TextStyle(fontSize: 48),
-              ),
-            ),
-          ],
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            GestureDetector(
-              onTap: (() => calcular('4'),
-              child: Text(
-                '4',
-                style: TextStyle(fontSize: 48),
-              ),
-            ),
-            GestureDetector(
-              onTap: (() => calcular('5'),
-              child: Text(
-                '5',
-                style: TextStyle(fontSize: 48),
-              ),
-            ),
-            GestureDetector(
-              onTap: (() => calcular('6'),
-              child: Text(
-                '6',
-                style: TextStyle(fontSize: 48),
-              ),
-            ),
-            GestureDetector(
-              onTap: (() => calcular('x'),
-              child: Text(
-                'x',
-                style: TextStyle(fontSize: 48),
-              ),
-            ),
-      ],
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            GestureDetector(
-              onTap: (() => calcular('1'),
-              child: Text(
-                '1',
-                style: TextStyle(fontSize: 48),
-              ),
-            ),
-            GestureDetector(
-              onTap: (() => calcular('2'),
-              child: Text(
-                '2',
-                style: TextStyle(fontSize: 48),
-              ),
-            ),
-            GestureDetector(
-              onTap: (() => calcular('3'),
-              child: Text(
-                '3',
-                style: TextStyle(fontSize: 48),
-              ),
-            ),
-            GestureDetector(
-              onTap: (() => calcular('-'),
-              child: Text(
-                '-',
-                style: TextStyle(fontSize: 48),
-              ),
-            ),
-          ],
-        ),
-      Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            GestureDetector(
-              onTap: (() => calcular('0'),
-              child: Text(
-                '0',
-                style: TextStyle(fontSize: 48),
-              ),
-            ),
-            GestureDetector(
-              onTap: (() => calcular(','),
-              child: Text(
-                ',',
-                style: TextStyle(fontSize: 48),
-              ),
-            ),
-            GestureDetector(
-              onTap: (() => calcular('='),
-              child: Text(
-                '=',
-                style: TextStyle(fontSize: 48),
-              ),
-            ),
-            GestureDetector(
-              onTap: (() => calcular('+'),
-              child: Text(
-                '+',
-                style: TextStyle(fontSize: 48),
-              ),
-            ),
-          ],
-        ),
   }
 }
